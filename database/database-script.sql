c4311b2e756e367c4eca3715b76f428e520662a4
Juan Marcos Ibarra <2994775760101@ingenieria.usac.edu.gt>
2022-05-20 04:47:21 +0000

    FIX table

--- database/database-script.sql
+++ database/database-script.sql
@@ -2,8 +2,8 @@ CREATE DATABASE ProyectoDocker;
 USE ProyectoDocker;
 
 CREATE TABLE IF NOT EXISTS Report (
-	'idReport' INT auto_increment PRIMARY KEY,
-    'id' VARCHAR(50),
-    'name' VARCHAR(100),
-    'password' VARCHAR(100),
+	`idReport` INT auto_increment PRIMARY KEY,
+    `id` VARCHAR(50),
+    `name` VARCHAR(100),
+    `password` VARCHAR(100)
 );
